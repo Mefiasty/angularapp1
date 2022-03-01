c9cc8a1b349e878069e32ec44eb3013575b7255a
Mefiasty <freshminter@gmail.com>
2022-03-01 18:11:28 +0100

    Auto-generated commit [ci skip]

---
+++ lista/app/app.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import { Task } from './task';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  editMode = false;
+  taskName = 'Sugerowane zadanie codzienne: odkurzanie';
+  taskDate = '';
+  config: { [key: string]: string } | null  = null;
+  tasks: Task[] = [
+    // {
+    //   name: 'test',
+    //   deadline: '2022-02-02',
+    //   done: false,
+    // },
+
+  ];
+
+  constructor() {
+    setTimeout(() => {
+      this.config = {
+        title: '🗓️Lista zadań🗓️',
+        footer: '© Lista zadań zbudowana w Angularze.',
+        date: new Date().toDateString(),
+      };
+    }, 500);
+    this.sortTasks();
+  }
+
+  clearTasks() {
+    this.tasks = [];
+  }
+
+  createTask() {
+    const task: Task = {
+      name: this.taskName,
+      deadline: this.taskDate,
+      done: false,
+    };
+    this.tasks.push(task);
+    this.taskName = '';
+    this.taskDate = '';
+    this.sortTasks();
+  }
+
+  switchEditMode(){
+    this.editMode = !this.editMode;
+  }
+
+  markTaskAsDone(task: Task) {
+    task.done = true;
+    this.sortTasks();
+  }
+
+  deleteTask(task: Task) {
+    this.tasks = this.tasks.filter(e => e !== task);
+    this.sortTasks();
+  }
+
+  private sortTasks() {
+    this.tasks = this.tasks.sort((a: Task, b: Task) =>
+      a.done === b.done ? 0 : a.done ? 1 : -1
+    );
+  }
+
+  title = '🗓️Lista zadań🗓️';
+  footer = '© Lista zadań zbudowana w Angularze.';
+  date = new Date().toDateString();
+
+}
